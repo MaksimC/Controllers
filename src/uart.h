c13d06d24627bf79301f8e650ccf340897c82c57
Maksim Tšeljabov <mtseljab@klass1-04-l.intra.itcollege.ee>
2016-11-18 20:04:39 +0200

    changes ready

--- src/uart.h
+++ src/uart.h
@@ -1,9 +1,9 @@
-int uart_putchar(char c, FILE *stream);
-int uart_getchar(FILE *stream);
+int uart0_putchar(char c, FILE *stream);
+int uart0_getchar(FILE *stream);
 int uart3_putchar(char    c,    FILE    *stream);
 
-void uart3_init(void);
-void uart_init(void);
+void uart0_initialize(void);
+void uart3_initialize(void);
 
 /* http://www.ermicro.com/blog/?p=325 */
 
